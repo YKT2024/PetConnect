84b1b9f4421d6a85192fc549d1c2bcb79104517d
a-yaka <otayk.hiamoe@outlook.jp>
2025-01-07 11:33:36 +0900

    ルート繋いだ(仮)

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,24 @@ Route::middleware('auth')->group(function () {
 // Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//↓仮のやつなのであとで消します：AYAKA
+Route::get('/posts', function () { return view('/posts/index_post'); });
+Route::get('/posts/create', function () { return view('/posts/create_post'); });
+Route::get('/posts/edit', function () { return view('/posts/edit_post'); });
+Route::get('/posts/show', function () { return view('/posts/show_post'); });
+
+Route::get('/pets', function () { return view('/pets/create_pet'); });
+Route::get('/pets/edit', function () { return view('pets/edit_pet'); });
+Route::get('/pets/show', function () { return view('pets/show_pet'); });
+Route::get('/pets/mypage', function () { return view('pets/mypage_pet'); });
+Route::get('/pets/hidden', function () { return view('/pets/hidden_pet'); });
+
+Route::get('/strays', function () { return view('/strays/index_stray'); });
+Route::get('/strays/create', function () { return view('/strays/create_stray'); });
+Route::get('/strays/edit', function () { return view('/strays/edit_stray'); });
+Route::get('/strays/show', function () { return view('/strays/show_stray'); });
+
+Route::get('/favorites', function () { return view('/favorites/index_favorite'); });
+//↑ここまで！
